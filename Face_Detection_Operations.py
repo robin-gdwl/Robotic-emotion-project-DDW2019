1d5bd7cd21cec3f0137ac524ae9d233cb707f375
Robin Godwyll <rodwyll+github@gmail.com>
2019-10-18 03:51:13 +0200

    minor adjustments to make it work on the raspberry

--- Face_Detection_Operations.py
+++ Face_Detection_Operations.py
@@ -5,7 +5,7 @@ import imutils
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
-from picamera import Picamera
+import picamera
 from picamera.array import PiRGBArray
 
 # parameters for loading data and images
@@ -31,7 +31,7 @@ class FaceOperation:
         self.screen_width = self.cap.get(3)   # x- extent of the captured frame
         self.screen_height = self.cap.get(4)  # y- extent
         self.face_loc = []
-        self.camera = PiCamera()
+        self.camera = picamera.PiCamera()
         self.camera.resolution = (1920, 1080)
         self.rawCapture = PiRGBArray(camera)
 
