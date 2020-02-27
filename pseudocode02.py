35b27854225d53b6af4ede0a003007816b48191f
Robin Godwyll <rodwyll+github@gmail.com>
2020-02-27 23:19:23 +0100

    Update pseudocode02.py

--- pseudocode02.py
+++ pseudocode02.py
@@ -76,8 +76,8 @@ if sys.platform == "linux":
     GPIO.setup(PAUSE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(PLAY_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(PAUSE_PIN, GPIO.RISING, callback=interrupt)
-    GPIO.add_event_detect(RESET_PIN, GPIO.RISING, callback=interrupt)
+    GPIO.add_event_detect(PAUSE_PIN, GPIO.BOTH, callback=interrupt)
+    GPIO.add_event_detect(RESET_PIN, GPIO.BOTH, callback=interrupt)
 
 vs = VideoStream(src= 0 ,
                  usePiCamera= RASPBERRY_BOOL,
