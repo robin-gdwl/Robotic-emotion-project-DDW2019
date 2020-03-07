79fa10a0665e406ce4e5ae7e97698f18e8760a45
Robin Godwyll <rodwyll+github@gmail.com>
2020-03-07 23:24:59 +0100

    Update programstate_test.py

--- programstate_test.py
+++ programstate_test.py
@@ -3,11 +3,12 @@ from PROGRAMSTATE import ProgramState
 
 programstate = ProgramState()
 
-programstate.level=0
-time.sleep(5)
-programstate.level=1
-time.sleep(5)
-programstate.level=2
-time.sleep(5)
-programstate.level=3
-time.sleep(5)
+while True:
+    programstate.level= 0
+    time.sleep(5)
+    programstate.level= 1
+    time.sleep(5)
+    programstate.level= 2
+    time.sleep(5)
+    programstate.level= 3
+    time.sleep(5)
