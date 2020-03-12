43d9ad23d370067a652f7c9e9f341f3506db5dd1
Robin Godwyll <rodwyll+github@gmail.com>
2020-03-12 17:02:45 +0100

    minor change to sync with raspberry pi

--- Videostream.py
+++ Videostream.py
@@ -6,9 +6,9 @@ import sys
 vs = VideoStream(src= 0 ,
                  usePiCamera= CONFIG.RASPBERRY_BOOL,
                  resolution=CONFIG.VIDEO_RESOLUTION,
-                 framerate = 13,
-                 meter_mode = "backlit",
+                 framerate = 12,
+                 meter_mode = "auto",
                  exposure_mode ="auto",
-                 shutter_speed = 8900,
-                 exposure_compensation = 2,
+                 shutter_speed = 5900,
+                 exposure_compensation = 3,
                  rotation = 180).start()
